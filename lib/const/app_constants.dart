998f2bd4a3775a5b40674220ec65f1d31061d3fc
imyya <gucciass68@gmail.com>
2025-10-24 00:18:25 +0000

    Remove API key from repository

--- lib/const/app_constants.dart
+++
@@ -1,7 +0,0 @@
-import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
-
-class AppConstants {
-  static const weather_key = "2856e7ab04824adccbc9e70889df7502";
-  static const OPENAI_API_KEY =
-      "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";
-}
